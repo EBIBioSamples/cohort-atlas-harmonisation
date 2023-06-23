ce4e6ccd78e73773e8fe531cff0f63eea0e9b7d1
mansurova <mansurova.m94@gmail.com>
2023-06-23 04:10:45 +0100

    using docker-compose.yml file

--- docker_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source ./env
-
-docker build -t cohort-atlas-harmonisation .
-
-lsof -i :${FLASK_PORT}
-
-docker run -d -p ${EXT_PORT}:${FLASK_PORT} -v $(pwd)/shared:/app/shared --name coh-atlas cohort-atlas-harmonisation
